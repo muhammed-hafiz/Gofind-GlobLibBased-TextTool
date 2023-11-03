d94d53260065a63889f6b127252786dda70df74a
muhammed-hafiz <m.hafizabuagla@gmail.com>
2023-11-04 00:23:24 +0200

    simple test script added

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 CFLAGS = -g -Wall -DNDEBUG
 
+SOURCES=src/gofind.c src/dbg.h src/simple_header.h
+all: $(SOURCES)
+	@mkdir bin	
+	gcc $(CFLAGS) $(SOURCES) -o bin/gofind
 clean:
-	rm gofind
+	@rm -r bin/
 
 
 	
--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@ echo Removing old build...
 make clean; echo
 
 echo Compiling...
-make gofind; echo
+make all; echo
 
 echo Unit Test...
 
 echo case 1
-if  ./gofind ali hello | grep -q '/home/circle/Desktop/c.txt' 
+if  ./bin/gofind ali hello | grep -q 'tests/path_c/c.txt' 
  then
 	echo passed; echo
 else echo faild; echo
@@ -15,29 +15,29 @@ fi
 
 
 echo case 2  
-if ./gofind noha | grep -q "/home/circle/Desktop/d.txt"
+if ./bin/gofind noha | grep -q "tests/path_c/path_d/d.txt"
  then
 	echo passed; echo
 else echo faild; echo
 fi
 
 echo case 3
-if ./gofind limia | grep -q "/home/circle/Desktop/d.txt"
+if ./bin/gofind limia | grep -q "tests/path_c/path_d/d.txt"
  then
 	echo passed; echo
 else echo faild; echo
 fi
 
 echo case 4 
-if ! ./gofind limia hafiz | grep -q ""
+if ! ./bin/gofind limia hafiz | grep -q ""
  then
 	echo passed; echo
 else echo faild; echo
 fi
 
 echo case 5
-if ./gofind -o limia hafiz | grep -q "/home/circle/Desktop/c.txt
-/home/circle/Desktop/d.txt"
+if ./bin/gofind -o limia hafiz | grep -q "tests/path_c/c.txt
+	tests/path_c/path_d/d.txt"
  then
 	echo passed; echo
 else echo faild; echo
